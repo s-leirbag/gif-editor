015bfe70c88d52125e0a37bccf95c971e1675615
Gabriel Shiu <gabriel@shiu.us>
2023-05-11 15:30:07 -0400

    tried fixing cors with express not vercel config

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,6 +5,17 @@ const Jimp = require('jimp');
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://gif-editor.vercel.app/"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  next();
+});
+
 router.post("/", upload.array("images", 5), async (req, res) => {
   let images = []
   for (const file of req.files) {
