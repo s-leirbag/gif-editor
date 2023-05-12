f4db096ec0bb5631dacbf3d839b7b2858826adb5
Gabriel Shiu <gabriel@shiu.us>
2023-05-11 22:11:43 -0400

    added sharp to try for speed. jimp greyscale test.

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
 const Jimp = require('jimp');
+const sharp = require('sharp');
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
@@ -9,9 +10,15 @@ router.post("/", upload.array("images", 50), async (req, res) => {
   let images = []
   for (const file of req.files) {
     const buffer = file.buffer;
+
+    // const a = await sharp(buffer).greyscale().toBuffer();
+    // const base64Image = 'data:image/png;base64,' + a.toString("base64");
+    
     const image = await Jimp.read(buffer);
-    base64Image = await image.invert().getBase64Async(Jimp.AUTO)
-    images.push(base64Image)
+    const base64Image = await image.greyscale().getBase64Async(Jimp.AUTO);
+
+    // console.log(base64Image);
+    images.push(base64Image);
   }
   res.send(images)
 });
