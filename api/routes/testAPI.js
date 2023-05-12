d99e2067fea1a3d87681462c2daa8f99521a7383
Gabriel Shiu <gabriel@shiu.us>
2023-05-11 20:59:17 -0400

    added get functionality to testapi route to test

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,6 +5,10 @@ const Jimp = require('jimp');
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
+router.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 router.post("/", upload.array("images", 50), async (req, res) => {
   let images = []
   for (const file of req.files) {
