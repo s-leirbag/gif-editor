ce26be52a96c9e799a5136767739478f7f229294
Gabriel Shiu <gabriel@shiu.us>
2023-05-11 21:07:29 -0400

    added route no router for testing on vercel

--- api/app.js
+++ api/app.js
@@ -22,6 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/asdf', (req, res) => {
+  res.end('asdf');
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
