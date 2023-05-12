d486ab3f42ac52f77c4d43c80e13fc3b6a65324c
Gabriel Shiu <gabriel@shiu.us>
2023-05-12 08:38:12 -0400

    no server pings. initial frontend layout. display and select images. highlight selected image.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+// import Typography from '@mui/material/Typography';
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 
-import { clone, cloneDeep, sample, isEmpty } from 'lodash';
+import { cloneDeep } from 'lodash';
+// import { clone, sample, isEmpty } from 'lodash';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,6 +19,7 @@ export default class App extends React.Component {
       face: '',
       images: [],
       filename: '',
+      selectedImg: null,
     };
   }
 
@@ -41,90 +43,131 @@ export default class App extends React.Component {
     if (!files)
       return;
 
-    // function readFile(file) {
-    //   return new Promise(function (resolve, reject) {
-    //     let fr = new FileReader();
-    //     fr.onload = function(){ resolve(fr.result) };
-    //     fr.onerror = function(){ reject(fr) };
-    //     fr.readAsDataURL(file);
-    //   });
-    // }
+    function readFile(file) {
+      return new Promise(function (resolve, reject) {
+        let fr = new FileReader();
+        fr.onload = function(){ resolve(fr.result) };
+        fr.onerror = function(){ reject(fr) };
+        fr.readAsDataURL(file);
+      });
+    }
 
-    // let readers = [];
-    // // Store promises in array
-    // for(const file of files){
-    //   readers.push(readFile(file));
-    // }
+    // Store promises in array
+    let readers = [];
+    for(const file of files){
+      readers.push(readFile(file));
+    }
     
-    // // Trigger Promises
-    // Promise.all(readers).then((urls) => {
-    //   let newImages = cloneDeep(this.state.images);
-    //   for (const url of urls)
-    //     newImages.push(url);
-    //   this.setState({ images: newImages })
-    // });
-    
-    let data = new FormData()
-    for (const file of e.target.files)
-      data.append('images', file)
-
-    const response = await fetch('/testAPI', {
-      method: "POST",
-      body: data,
+    // Trigger Promises
+    Promise.all(readers).then((urls) => {
+      let newImages = cloneDeep(this.state.images);
+      for (const url of urls)
+        newImages.push(url);
+      this.setState({ images: newImages })
+      if (this.state.selectedImg == null) {
+        this.setState({ selectedImg: 0 });
+      }
     });
-    if (!response.ok)
-      return;
+    
+    // let data = new FormData()
+    // for (const file of e.target.files)
+    //   data.append('images', file)
 
-    let newImages = cloneDeep(this.state.images);
-    const images = await response.json();
-    for (const image of images)
-      newImages.push(image);
-    this.setState({ images: newImages })
+    // const response = await fetch('/testAPI', {
+    //   method: "POST",
+    //   body: data,
+    // });
+    // if (!response.ok)
+    //   return;
+
+    // let newImages = cloneDeep(this.state.images);
+    // const images = await response.json();
+    // for (const image of images)
+    //   newImages.push(image);
+    // this.setState({ images: newImages })
+  }
+
+  handleClickImage(i) {
+    this.setState({ selectedImg: i });
+  }
+
+  renderImg(i) {
+    // Highlight image if selected
+    let bgCol;
+    if (this.state.selectedImg === i) {
+      bgCol = 'yellow';
+    }
+    
+    return (
+      <Box
+        component='img'
+        key={i}
+        src={this.state.images[i]}
+        alt={'image ' + i}
+        loading='lazy'
+        sx={{ width: '100%', borderColor: bgCol, borderWidth: 2, borderStyle: 'solid' }}
+        onClick={() => this.handleClickImage(i)}
+      />
+    );
   }
 
   render() {
     const face = this.state.face ? <img
       src={this.state.face}
-      // width={'100%'}
+      width={'100%'}
       alt={'face'}
+      loading="lazy"
+    /> : '';
+    const selectedImg = this.state.selectedImg != null ? <img
+      src={this.state.images[this.state.selectedImg]}
+      width={'100%'}
+      alt={'selected'}
       loading="lazy"
     /> : '';
     return (
       <div className="App">
         <CssBaseline />
-        <Paper sx={{ m: 2, p: 2 }}>
-          <Button
-            component="label"
-            variant="outlined"
-            startIcon={<UploadFileIcon />}
-            sx={{ marginRight: "1rem" }}
-          >
-            Upload Face
-            <input type="file" accept="image/*" hidden onChange={(e) => this.handleFaceUpload(e)} />
-          </Button>
-          <Button
-            component="label"
-            variant="outlined"
-            startIcon={<UploadFileIcon />}
-            sx={{ marginRight: "1rem" }}
-          >
-            Upload Images
-            <input type="file" accept="image/*" multiple hidden onChange={(e) => this.handleImagesUpload(e)} />
-          </Button>
-          {face}
-          <ImageList sx={{ height: '100%' }} cols={3} >
-            {this.state.images.map((image, index) => (
-              <ImageListItem key={index}>
-                <img
-                  src={image}
-                  width={'100%'}
-                  alt={'image ' + index}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </Paper>
+        <Grid container columnSpacing={2} sx={{ p: 2, width: '100vw', height: '100vh' }}>
+          <Grid item xs={5}>
+            <Paper sx={{ p: 2, height: '100%' }} elevation={4}>
+              <Box>
+                <Button
+                  component="label"
+                  variant="outlined"
+                  startIcon={<UploadFileIcon />}
+                  sx={{ marginRight: "1rem" }}
+                >
+                  Upload Face
+                  <input type="file" accept="image/*" hidden onChange={(e) => this.handleFaceUpload(e)} />
+                </Button>
+                <Button
+                  component="label"
+                  variant="outlined"
+                  startIcon={<UploadFileIcon />}
+                  sx={{ marginRight: "1rem" }}
+                >
+                  Upload Images
+                  <input type="file" accept="image/*" multiple hidden onChange={(e) => this.handleImagesUpload(e)} />
+                </Button>
+              </Box>
+              {face}
+            </Paper>
+          </Grid>
+          <Grid item xs={5} sx={{ height: '100%' }}>
+            <Paper sx={{ p: 2, height: '100%' }} elevation={4}>
+              {selectedImg}
+            </Paper>
+          </Grid>
+          <Grid item xs={2} sx={{ height: '100%' }}>
+            <Paper sx={{ p: 2, height: '100%' }} elevation={4}>
+              <Box sx={{ height: '100%', display: 'block', overflow: 'auto' }}>
+                {this.state.images.map((image, index) => (
+                  this.renderImg(index)
+                ))}
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
