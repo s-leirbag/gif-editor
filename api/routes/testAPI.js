40b62cb2f9555641fc84c57c44aae62f5ca5517a
Gabriel Shiu <gabriel@shiu.us>
2023-05-11 22:26:51 -0400

    bye bye jimp, changed icons and title and desc

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer')
-const Jimp = require('jimp');
 const sharp = require('sharp');
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
@@ -10,15 +9,9 @@ router.post("/", upload.array("images", 50), async (req, res) => {
   let images = []
   for (const file of req.files) {
     const buffer = file.buffer;
-
-    const a = await sharp(buffer).greyscale().toBuffer();
-    const base64Image = 'data:image/png;base64,' + a.toString("base64");
-    
-    // const image = await Jimp.read(buffer);
-    // const base64Image = await image.greyscale().getBase64Async(Jimp.AUTO);
-
-    // console.log(base64Image);
-    images.push(base64Image);
+    const newImage = await sharp(buffer).greyscale().toBuffer();
+    const base64 = 'data:image/png;base64,' + newImage.toString("base64");
+    images.push(base64);
   }
   res.send(images)
 });
