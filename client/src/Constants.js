5f30097ea37173667136730baa0a80ab07490b7d
Gabriel Shiu <gabriel@shiu.us>
2024-05-12 19:50:44 -0400

    high res chimmy hearts sample gif

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,6 +1,6 @@
 export const sampleFaces = [ 'doge', 'lego_head', 'mad_laugh', 'meme_man', 'smirk', 'steve', 'zucc' ];
 export const sampleGifs = [ 'dream', 'nyan_cat', 'roblox', 'doomed', 'exclamation', 'good', 'sigh' ];
-export const sampleGifsHighRes = [ 'high_res/doomed', 'high_res/exclamation', 'high_res/good', 'high_res/sigh' ];
+export const sampleGifsHighRes = [ 'high_res/doomed', 'high_res/exclamation', 'high_res/good', 'high_res/sigh', 'high_res/chimmy_hearts' ];
 export const sampleGifCounts = {
   'dream': 16,
   'doomed': 18,
@@ -13,4 +13,5 @@ export const sampleGifCounts = {
   'high_res/good': 28,
   'high_res/doomed': 18,
   'high_res/sigh': 11,
+  'high_res/chimmy_hearts': 16,
 };
