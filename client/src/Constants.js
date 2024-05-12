bf3373d39f1a95f8162c75fa92110f49ec0829e7
Gabriel Shiu <gabriel@shiu.us>
2024-05-12 19:11:31 -0400

    high res sigh sample gif

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,6 +1,6 @@
 export const sampleFaces = [ 'doge', 'lego_head', 'mad_laugh', 'meme_man', 'smirk', 'steve', 'zucc' ];
 export const sampleGifs = [ 'dream', 'nyan_cat', 'roblox', 'doomed', 'exclamation', 'good', 'sigh' ];
-export const sampleGifsHighRes = [ 'high_res/doomed', 'high_res/exclamation', 'high_res/good' ];
+export const sampleGifsHighRes = [ 'high_res/doomed', 'high_res/exclamation', 'high_res/good', 'high_res/sigh' ];
 export const sampleGifCounts = {
   'dream': 16,
   'doomed': 18,
@@ -12,4 +12,5 @@ export const sampleGifCounts = {
   'high_res/exclamation': 20,
   'high_res/good': 28,
   'high_res/doomed': 18,
+  'high_res/sigh': 11,
 };
